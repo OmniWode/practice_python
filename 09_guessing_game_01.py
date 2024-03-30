70234c7808e88c89c20864c9aebd4740443f27c4
Parker <omniwode@gmail.com>
2024-03-30 09:29:05 -0400

    added extras and input validation to exercise 09

--- 09_guessing_game_01.py
+++ 09_guessing_game_01.py
@@ -8,15 +8,35 @@ win = False
 print('I\'m thinking of a number between 1 and 9, inclusive.')
 print('See if you can guess it!')
 
-while win == False:
-    guess = int(input('Your guess : '))
+#extra 1
+print('Game will continue until you type "exit"')
 
-    if guess < goal:
-        print('Guess is too low! Try again')
-    elif guess > goal:
-        print('Guess is too high! Try again')
-    else:
-        print ('You guessed my number! Well done')
+#extra 2
+guess_count = 0
+
+while win == False:
+    guess = input('Your guess : ')
+
+    if guess == 'exit':
         win = True
+    elif (guess.isdigit()):
+        if int(guess) < goal:
+            guess_count += 1
+            print('Guess is too low! Try again')
+        elif int(guess) > goal:
+            guess_count += 1
+            print('Guess is too high! Try again')
+        elif int(guess) == goal:
+            #extra 2
+            guess_count += 1
+            print (f'You guessed my number! Well done, it took you {guess_count} tries to guess')
+            print('Let\'s play again! I\'ll think of a new number and reset the guess count...')
+            print('Ready!')
+
+            goal = random.randint(1,9)
+            guess_count = 0
+    else:
+        print('Invalid input, please try again')
+
 
 print('GAME OVER')
