4d518e43d232c2af40e5451d034a8778c4c8ae24
Parker <omniwode@gmail.com>
2024-04-04 19:19:08 -0400

    ex15 - completed solution

--- 15_reverse_word_order.py
+++ 15_reverse_word_order.py
@@ -1,2 +1,11 @@
 #15_reverse_word_order.py
 
+def rev_word(sentence):
+    temp = sentence.split()
+    temp.reverse()
+    return ' '.join(temp)
+
+print('Please enter a phrase with mlutiple words:')
+string = input('>>> ')
+reverse_string = rev_word(string)
+print(reverse_string)
