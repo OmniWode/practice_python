8355172ca85d850b6d352a017a632813bd0ea6a4
Parker <omniwode@gmail.com>
2024-04-11 17:48:01 -0400

    ex17 - need to revisit

--- 17_decode_a_webpage.py
+++ 17_decode_a_webpage.py
@@ -1,2 +1,18 @@
 #17_decode_a_webpage.py
 
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://nytimes.com'
+r = requests.get(url)
+r_html = r.text
+
+
+soup = BeautifulSoup(r_html)
+
+for story_heading in soup.find_all(class_='story-heading'):
+	if story_heading.a:
+		print(story_heading.a.text.replace('\n', ' ').strip())
+	else:
+		print(story_heading.contents[0].strip())
