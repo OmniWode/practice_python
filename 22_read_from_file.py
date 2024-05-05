3ff5cbe1109c44a9d69bf86cd85b81016236c8c1
Parker <omniwode@gmail.com>
2024-05-05 19:57:42 -0400

    ex22 - solution with extra

--- 22_read_from_file.py
+++ 22_read_from_file.py
@@ -11,19 +11,39 @@ def count_names(name_list):
 
 	return name_count
 
+def count_categories(scene_list):
+	category_count = {}
+
+	for scene in scene_list:
+		category = scene[3:-26]
+		if category in category_count:
+			category_count[category] += 1
+		else:
+			category_count[category] = 1
+
+	return category_count
+
 def main():
 	path = 'files/'
-	file = path + 'nameslist.txt'
+	#file = path + 'nameslist.txt'
+	file = path + 'categories.txt'
 
 	print(file)
 
 	with open(file,'r') as open_file:
 		all_text = open_file.read()
 
-	name_list = all_text.split()
+	string_list = all_text.split()
 
-	name_count = count_names(name_list)
-	print(name_count)
+	#name_count = count_names(string_list)
+	#print(name_count)
+
+
+	# extra
+	category_count = count_categories(string_list)
+	print(category_count)
+
+
 
 if __name__ == '__main__':
 	main()
