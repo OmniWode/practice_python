ba8cd9e4316dd5778bb218902e59a44f3daa14ef
Parker <omniwode@gmail.com>
2024-05-05 19:44:42 -0400

    ex22 - solution

--- 22_read_from_file.py
+++ 22_read_from_file.py
@@ -1,8 +1,29 @@
 #22_read_from_file.py
 
+def count_names(name_list):
+	name_count = {}
+
+	for name in name_list:
+		if name in name_count:
+			name_count[name] += 1
+		else:
+			name_count[name] = 1
+
+	return name_count
 
 def main():
-	
+	path = 'files/'
+	file = path + 'nameslist.txt'
+
+	print(file)
+
+	with open(file,'r') as open_file:
+		all_text = open_file.read()
+
+	name_list = all_text.split()
+
+	name_count = count_names(name_list)
+	print(name_count)
 
 if __name__ == '__main__':
 	main()
